c31492681fbb36880d0a7962b906fea1b3cf6f6d
22-YanKaiky <yankaikys@gmail.com>
2021-10-23 17:27:00 -0300

    .env

---
+++ src/models/index.js
@@ -0,0 +1,12 @@
+const Sequelize = require("Sequelize");
+const sequelize = new Sequelize('cinemovie', 'root', '123456', {
+    host: "localhost",
+    dialect: "mysql"
+});
+
+try {
+    sequelize.authenticate()
+    console.log("Conectado com sucesso")
+} catch(error) {
+    console.log("Falha ao se conectar", error)
+}
